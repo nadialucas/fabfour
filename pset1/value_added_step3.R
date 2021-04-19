5c4d99d25f812ec6b021f67847823413be61b8cc
Nadia Lucas <nlucas040@gmail.com>
2021-04-19 16:49:34 -0500

    fixed up some things, just gotta get the mus now

--- pset1/value_added_step3.R
+++ pset1/value_added_step3.R
@@ -17,7 +17,10 @@ library(tidyverse)
 ddir <- "/Users/nadialucas/Documents/fabfour/pset1"
 setwd(ddir)
 
-var_matrix <- read.csv("var-matrix.csv", stringsAsFactors=TRUE)
+var_matrix <- read.csv("var-matrix.csv") %>%
+  mutate(vars = substr(c1, start = 4, stop = str_length(c1)-2)) %>%
+  mutate(vars = as.numeric(vars)) %>%
+  select(vars)
 
 # currently this step is broken 
 teacher_df <- read.csv("va-class-level.csv")
@@ -27,15 +30,15 @@ teacher_df <- read.csv("va-class-level.csv")
 #========================================================================#
 
 # for each teacher construct a sigma 
-sigma_A <- var_matrix[1]
-sigma_eps <- var_matrix[2]
-sigma_theta <- var_matrix[3]
-sig1 <- var_matrix[4]
-sig2 <- var_matrix[5]
-sig3 <- var_matrix[6]
-sig4 <- var_matrix[7]
-sig5 <- var_matrix[8]
-sig6 <- var_matrix[9]
+sigma_A <- var_matrix[1,1]
+sigma_eps <- var_matrix[2,1]
+sigma_theta <- var_matrix[3,1]
+sig1 <- var_matrix[4,1]
+sig2 <- var_matrix[5,1]
+sig3 <- var_matrix[6,1]
+sig4 <- var_matrix[7,1]
+sig5 <- var_matrix[8,1]
+sig6 <- var_matrix[9,1]
 
 Cov_list <- c(0, sig1, sig2, sig3, sig4, sig5, sig6)
 
